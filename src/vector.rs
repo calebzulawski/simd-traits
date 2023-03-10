548660dbabd661d971d434bbff7bab288de74bd4
Caleb Zulawski <caleb.zulawski@gmail.com>
2023-03-10 14:31:33 -0500

    Rename lane -> element

--- src/vector.rs
+++ src/vector.rs
@@ -5,21 +5,21 @@ use core::simd::{LaneCount, Mask, MaskElement, Simd, SimdElement, SupportedLaneC
 /// This is not necessarily a specific hardware type, but any kind of parallel collection.
 pub trait Vector: Sized {
     type Scalar;
-    const LANES: usize;
+    const ELEMENTS: usize;
 
     fn splat(value: Self::Scalar) -> Self;
 
-    unsafe fn extract_unchecked(&self, lane: usize) -> Self::Scalar;
-    unsafe fn insert_unchecked(&mut self, lane: usize, value: Self::Scalar);
+    unsafe fn extract_unchecked(&self, index: usize) -> Self::Scalar;
+    unsafe fn insert_unchecked(&mut self, index: usize, value: Self::Scalar);
 
-    fn extract(&self, lane: usize) -> Self::Scalar {
-        assert!(lane < Self::LANES);
-        unsafe { self.extract_unchecked(lane) }
+    fn extract(&self, index: usize) -> Self::Scalar {
+        assert!(index < Self::ELEMENTS);
+        unsafe { self.extract_unchecked(index) }
     }
 
-    fn insert(&mut self, lane: usize, value: Self::Scalar) {
-        assert!(lane < Self::LANES);
-        unsafe { self.insert_unchecked(lane, value) }
+    fn insert(&mut self, index: usize, value: Self::Scalar) {
+        assert!(index < Self::ELEMENTS);
+        unsafe { self.insert_unchecked(index, value) }
     }
 }
 
@@ -29,18 +29,18 @@ where
     LaneCount<N>: SupportedLaneCount,
 {
     type Scalar = T;
-    const LANES: usize = N;
+    const ELEMENTS: usize = N;
 
     fn splat(value: Self::Scalar) -> Self {
         Simd::splat(value)
     }
 
-    unsafe fn extract_unchecked(&self, lane: usize) -> Self::Scalar {
-        *self.as_array().get_unchecked(lane)
+    unsafe fn extract_unchecked(&self, index: usize) -> Self::Scalar {
+        *self.as_array().get_unchecked(index)
     }
 
-    unsafe fn insert_unchecked(&mut self, lane: usize, value: Self::Scalar) {
-        *self.as_mut_array().get_unchecked_mut(lane) = value;
+    unsafe fn insert_unchecked(&mut self, index: usize, value: Self::Scalar) {
+        *self.as_mut_array().get_unchecked_mut(index) = value;
     }
 }
 
@@ -50,17 +50,17 @@ where
     LaneCount<N>: SupportedLaneCount,
 {
     type Scalar = bool;
-    const LANES: usize = N;
+    const ELEMENTS: usize = N;
 
     fn splat(value: Self::Scalar) -> Self {
         Mask::splat(value)
     }
 
-    unsafe fn extract_unchecked(&self, lane: usize) -> Self::Scalar {
-        self.test_unchecked(lane)
+    unsafe fn extract_unchecked(&self, index: usize) -> Self::Scalar {
+        self.test_unchecked(index)
     }
 
-    unsafe fn insert_unchecked(&mut self, lane: usize, value: Self::Scalar) {
-        self.set_unchecked(lane, value)
+    unsafe fn insert_unchecked(&mut self, index: usize, value: Self::Scalar) {
+        self.set_unchecked(index, value)
     }
 }
